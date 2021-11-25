dbd18b18cc4a14438665f56adf14ef9f32e19fcc
jagratac <jagrat.acharya@intel.com>
2021-11-25 13:23:16 +0530

    Updated log files and snapshot of wlc output

--- destroy_VMs.sh
+++ destroy_VMs.sh
@@ -1,9 +1,7 @@
 for ((i=0;i<$1;i++)); 
 do 
-   # your-unix-command-here
-    echo $i
     virsh destroy ubuntu-vm-$i
     virsh undefine ubuntu-vm-$i
-    #rm vm_$i.xml
+    echo VM $i deleted
 done
 
