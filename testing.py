42099cd304beddb2fb71e1dd096f01337e025c29
jagrat acharya <jagrat.acharya@intel.com>
2021-12-21 12:46:18 +0530

    RTC TASK:- 12345-commit templets
    Problem:-modification to commit templet
    
    Solution:-addred RTC id
    
    Note:-no special note

--- testing.py
+++ testing.py
@@ -1,2 +1,3 @@
 print ("this file i added to test pull request templets changes ")
 print ("this file i added to test commit templets changes ")
+print ("this file i added to test commit templets improvement")
